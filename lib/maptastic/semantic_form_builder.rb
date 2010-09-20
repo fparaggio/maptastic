5311ef1394de1d24c780c44ff92661caa70047cb
jimmy <pake007@gmail.com>
2010-09-20 12:31:43 +0800

    Add the zoom option, and refactor the maptastic.js to provide some public API of MaptasticMap object. Add the geocoder function.

--- lib/maptastic/semantic_form_builder.rb
+++ lib/maptastic/semantic_form_builder.rb
@@ -1,6 +1,6 @@
 module Maptastic
   class SemanticFormBuilder < Formtastic::SemanticFormBuilder
-    
+
     def multi_input(*args)
       options = args.extract_options!
 
@@ -10,36 +10,37 @@ module Maptastic
         args.inject('') {|html, arg| html << input(arg, options)}
       end
     end
-    
+
   private
-    
+
     def map_div_id(methods)
       generate_html_id(methods.map(&:to_s).join('_') << '_map')
     end
-    
+
     def map_input_id(method)
       generate_html_id("map_#{method}")
     end
 
-    def map_js(methods)
+    def map_js(methods, zoom)
       @template.content_tag("script", :lang => "javascript") do
         "
-new MaptasticMap({
-  mapId: '#{map_div_id(methods)}',
-  latInput: '#{map_input_id(methods.first)}', 
-  lngInput: '#{map_input_id(methods.last)}'
-});
+MaptasticMap.init({
+    mapId: '#{map_div_id(methods)}',
+    latInput: '#{map_input_id(methods.first)}',
+    lngInput: '#{map_input_id(methods.last)}',
+    zoom : '#{zoom}'
+  });
 "
       end
     end
-    
+
     def map_input(methods, options = {})
       options[:hint] ||= "Click to select a location, then drag the marker to position"
       inputs_html = methods.inject('') {|html, method| html << input(method, :id => map_input_id(method), :as => :hidden)}
       hint_html = inline_hints_for(methods.first, options)
       map_container = @template.content_tag(:div, nil, :class => 'map', :id => map_div_id(methods))
-      map_html = @template.content_tag(:li,  Formtastic::Util.html_safe("#{map_container} #{hint_html.to_s} #{options[:skip_js] == true ? '' : map_js(methods).to_s}"))
-      
+      map_html = @template.content_tag(:li,  Formtastic::Util.html_safe("#{map_container} #{hint_html.to_s} #{options[:skip_js] == true ? '' : map_js(methods, options[:zoom]).to_s}"))
+
       Formtastic::Util.html_safe(inputs_html + map_html)
     end
 
